8e4ae5d7071286a21043a7361d3d5f1240eb1c83
POO <bryan.angulo01@epn.edu.ec>
2025-04-24 09:58:16 -0500

    objeto 1 Bryan

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,11 @@ public class Main {
 //Crear la instancia vehiculo
         Vehiculos vehiculo1=new Vehiculos("azul","pbc1234","sedan",2020);
         Vehiculos vehiculo2=new Vehiculos("rojo","pbc1254","kia",2021);
+        Vehiculos vehiculo3 =new Vehiculos("Azul","pbc7894","kia",2005);
         vehiculo1.Informacion();
         vehiculo2.Informacion();
-        Vehiculos v1 = new Vehiculos("Azul",7894,"kia",2005);
-        System.out.println(v1);
+        vehiculo3.Informacion();
+
 
     }
 
