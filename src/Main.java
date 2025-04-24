78efd18876d32ada99ccbcc5fa298af03dd9dd39
POO <xavier.guanoluisa@epn.edu.ec>
2025-04-24 09:47:18 -0500

    Actualizacion v2

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,11 @@ public class Main {
     Vehiculos v1 = new Vehiculos("Azul",7894,"kia",2005);
     System.out.println(v1);
 
+    //
+//Crear la instancia empleado
+        Vehiculos vehiculo1=new Vehiculos("azul","pbc1234","sedan",2020);
+        Vehiculos vehiculo2=new Vehiculos("rojo","pbc1254","kia",2021);
+        vehiculo1.Informacion();
+        vehiculo2.Informacion();
     }
 }
