5d2d7e93a8327d866c45eaa8c27835c5d164e897
POO <bryan.angulo01@epn.edu.ec>
2025-04-24 09:18:05 -0500

    creacion clase Bryan

--- src/Vehiculos.java
+++ src/Vehiculos.java
@@ -3,5 +3,17 @@ public class Vehiculos {
     public String color;
     public int placa;
     public String modelo;
+    public int anio;
+
+    public Vehiculos(String color, int placa, String modelo, int anio) {
+        this.color = color;
+        this.placa = placa;
+        this.modelo = modelo;
+        this.anio = anio;
+
+
+
+    }
+
 
 }
