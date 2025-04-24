900da9594f506f8145bea775a51cd9e5a4647584
POO <bryan.angulo01@epn.edu.ec>
2025-04-24 09:39:21 -0500

    objeto 1 Bryan

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@
 public class Main {
     public static void main(String[] args) {
     //
-
+//Crear la instancia empleado
+        Vehiculos vehiculo1=new Vehiculos("azul","pbc1234","sedan",2020);
     }
 }
